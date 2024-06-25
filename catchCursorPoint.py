81004982a9d851369895ef8da7f6b04514bc7ef9
in-veloper <yiniwinidev@gmail.com>
2024-06-26 00:12:29 +0900

    6.26 pyqt 변경 commit

--- catchCursorPoint.py
+++ catchCursorPoint.py
@@ -1,24 +1,127 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import chromedriver_autoinstaller
-from selenium.webdriver.common.action_chains import ActionChains
-from pynput import keyboard
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QFrame, QMessageBox
+from PyQt5.QtCore import Qt, QTimer, QThread
 import pyautogui
 import pyperclip
-import time
-import ssl
 import os
-from threading import Thread
-import tkinter as tk
-from tkinter import messagebox
+from pynput import keyboard
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
-# SSL 인증서 처리 
-ssl._create_default_https_context = ssl._create_unverified_context
-# Chrome Driver 환경에 맞게 자동 Install
-chromedriver_autoinstaller.install()
+class KeyboardListenerThread(QThread):
+    def run(self):
+        start_listening()
 
-# 위치 좌표 저장할 Directory 획득 Def
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Catch Cursor Point")
+        screen_width, screen_height = pyautogui.size()
+
+        window_width = 500
+        window_height = 350
+        x_position = screen_width - window_width
+        y_position = (screen_height - window_height) // 2
+
+        self.setGeometry(x_position, y_position, window_width, window_height)
+        self.setStyleSheet("background-color: #f0f0f0;")
+
+        self.layout = QVBoxLayout()
+
+        self.userId_frame = QFrame(self)
+        self.userId_label = QLabel("ID : ", self.userId_frame)
+        self.userId_label.setStyleSheet("color: black;")
+        self.userId_entry = QLineEdit(self.userId_frame)
+        self.userId_entry.setStyleSheet("color: black; background-color: white;")
+        self.layout.addWidget(self.userId_label)
+        self.layout.addWidget(self.userId_entry)
+
+        self.password_frame = QFrame(self)
+        self.password_label = QLabel("PW : ", self.password_frame)
+        self.password_label.setStyleSheet("color: black;")
+        self.password_entry = QLineEdit(self.password_frame)
+        self.password_entry.setEchoMode(QLineEdit.Password)
+        self.password_entry.setStyleSheet("color: black; background-color: white;")
+        self.layout.addWidget(self.password_label)
+        self.layout.addWidget(self.password_entry)
+
+        self.browser_button = QPushButton("브라우저 열기", self)
+        self.browser_button.setStyleSheet("color: white; background-color: gray;")
+        self.browser_button.clicked.connect(self.launch_browser)
+        self.layout.addWidget(self.browser_button)
+
+        self.info_label = QLabel(
+            "F1 : 도움말 닫기 버튼\nF2 : 제목 입력 창\nF3 : 내용 입력 창\nF4 : 우측 상단 발행 버튼\nF5 : 최종 발행 버튼",
+            self)
+        self.info_label.setStyleSheet("border: 2px groove; background-color: white; color: black;")
+        self.layout.addWidget(self.info_label)
+
+        self.position_label = QLabel("현재 마우스 위치 : ", self)
+        self.position_label.setStyleSheet("border: 2px groove; background-color: white; color: black;")
+        self.layout.addWidget(self.position_label)
+
+        self.reset_button = QPushButton("Reset", self)
+        self.reset_button.setStyleSheet("color: white; background-color: gray;")
+        self.reset_button.clicked.connect(self.reset_data)
+        self.close_button = QPushButton("Close", self)
+        self.close_button.setStyleSheet("color: white; background-color: gray;")
+        self.close_button.clicked.connect(self.close_app)
+        self.layout.addWidget(self.reset_button)
+        self.layout.addWidget(self.close_button)
+
+        self.setLayout(self.layout)
+        self.update_mouse_position()
+
+    def launch_browser(self):
+        driver = webdriver.Chrome()
+        driver.get('https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0')
+        try:
+            start_button = driver.find_element(By.XPATH, '//*[@id="container"]/div/aside/div/div[1]/div[1]/div/a')
+            start_button.click()
+            time.sleep(2)
+            userId = self.userId_entry.text()
+            userId_field = driver.find_element(By.XPATH, '//*[@id="id"]')
+            userId_field.click()
+            pyperclip.copy(userId)
+            pyautogui.keyDown("command")
+            pyautogui.press("v")
+            pyautogui.keyUp("command")
+            time.sleep(2)
+            password = self.password_entry.text()
+            password_field = driver.find_element(By.XPATH, '//*[@id="pw"]')
+            password_field.click()
+            pyperclip.copy(password)
+            pyautogui.keyDown("command")
+            pyautogui.press("v")
+            pyautogui.keyUp("command")
+            password_field.send_keys(Keys.RETURN)
+            time.sleep(3)
+            write_button = driver.find_element(By.XPATH, '//*[@id="container"]/div/aside/div/div[1]/nav/a[2]')
+            action = ActionChains(driver)
+            action.move_to_element(write_button).click().perform()
+            input("마우스 커서를 움직여 좌표를 확인하세요")
+        finally:
+            driver.quit()
+
+    def reset_data(self):
+        file_path = os.path.join(get_data_directory(), 'naver_coordinate.txt')
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        QMessageBox.information(self, "데이터 리셋", "저장된 데이터가 초기화되었습니다.")
+
+    def update_mouse_position(self):
+        x, y = pyautogui.position()
+        self.position_label.setText(f"현재 마우스 위치 [ X : {x}, Y : {y} ]")
+        QTimer.singleShot(100, self.update_mouse_position)
+
+    def close_app(self):
+        self.close()
+
 def get_data_directory():
     home_dir = os.path.expanduser('~')
     data_dir = os.path.join(home_dir, 'capdata')
@@ -26,198 +129,60 @@ def get_data_directory():
         os.makedirs(data_dir)
     return data_dir
 
-def show_message(message):
-    root.after(0, lambda: messagebox.showinfo("정보", message))
-
-# 커서 위치 좌표 저장 Def
 def save_coordinates(x, y, label):
     file_path = os.path.join(get_data_directory(), 'naver_coordinate.txt')
     with open(file_path, "a") as file:
         file.write(f"{label}: {x}, {y}\n")
     if label == "existWrite":
-        show_message("이미 작성중인 글 버튼 좌표가 저장되었습니다.")
+        QMessageBox.information(None, "정보", "이미 작성중인 글 버튼 좌표가 저장되었습니다.")
     if label == "helpExit":
-        show_message("도움말 위치 좌표가 저장되었습니다.")
+        QMessageBox.information(None, "정보", "도움말 위치 좌표가 저장되었습니다.")
     if label == "title":
-        show_message("제목 위치 좌표가 저장되었습니다.")
+        QMessageBox.information(None, "정보", "제목 위치 좌표가 저장되었습니다.")
     if label == "content":
-        show_message("내용 위치 좌표가 저장되었습니다.")
+        QMessageBox.information(None, "정보", "내용 위치 좌표가 저장되었습니다.")
     if label == "publish":
-        show_message("발행 버튼 위치 좌표가 저장되었습니다.")
+        QMessageBox.information(None, "정보", "발행 버튼 위치 좌표가 저장되었습니다.")
     if label == "realPublish":
-        show_message("최종 발행 버튼 위치 좌표가 저장되었습니다.")
-
+        QMessageBox.information(None, "정보", "최종 발행 버튼 위치 좌표가 저장되었습니다.")
 
 def on_press(key):
     try:
-        if key == keyboard.Key.f1:       # 이미 작성중인 글 버튼 위치
+        if key == keyboard.Key.f1:
             label = "existWrite"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
-        if key == keyboard.Key.f2:       # 도움말 닫기 위치
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
+        if key == keyboard.Key.f2:
             label = "helpExit"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
-        if key == keyboard.Key.f3:       # 제목 입력창 위치
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
+        if key == keyboard.Key.f3:
             label = "title"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
-        if key == keyboard.Key.f4:       # 내용 입력창 위치
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
+        if key == keyboard.Key.f4:
             label = "content"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
-        if key == keyboard.Key.f5:       # 발행 버튼 위치
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
+        if key == keyboard.Key.f5:
             label = "publish"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
-        if key == keyboard.Key.f6:       # 최종 발행 버튼 위치
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
+        if key == keyboard.Key.f6:
             label = "realPublish"
-            x,y = pyautogui.position()
-            save_coordinates(x,y,label)
+            x, y = pyautogui.position()
+            save_coordinates(x, y, label)
     except Exception as e:
         print(f"Error: {e}")
 
-# 키 입력 Listener 시작 Def
 def start_listening():
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
 
-# 키 입력 Listener Run Def
-def run_listener():
-    listener_thread = Thread(target=start_listening, daemon=True)
+if __name__ == '__main__':
+    app = QApplication([])
+    listener_thread = KeyboardListenerThread()
     listener_thread.start()
-
-# 커서 좌표 저장 데이터 Reset Def
-def reset_data():
-    file_path = os.path.join(get_data_directory(), 'naver_coordinate.txt')
-    if os.path.exists(file_path):
-        os.remove(file_path)  # 파일 삭제
-    messagebox.showinfo("데이터 리셋", "저장된 데이터가 초기화되었습니다.")
-
-# 커서 위치 Update Def
-def update_mouse_position():
-    x, y = pyautogui.position()
-    position_label.config(text=f"현재 마우스 위치 [ X : {x}, Y : {y} ]")
-    root.after(100, update_mouse_position)
-
-# Browser 시작 Def
-def launch_browser():
-    global driver
-    driver = webdriver.Chrome()
-    driver.get('https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0')
-    
-    try:
-        start_button = driver.find_element(By.XPATH, '//*[@id="container"]/div/aside/div/div[1]/div[1]/div/a')
-        start_button.click()
-
-        time.sleep(2)
-
-        # 로그인 정보 입력
-        userId = userId_entry.get()
-        userId_field = driver.find_element(By.XPATH, '//*[@id="id"]')
-        userId_field.click()
-        pyperclip.copy(userId)
-        pyautogui.keyDown("command")
-        pyautogui.press("v")
-        pyautogui.keyUp("command")
-        time.sleep(2)
-
-        password = password_entry.get()
-        password_field = driver.find_element(By.XPATH, '//*[@id="pw"]')
-        password_field.click()
-        pyperclip.copy(password)
-        pyautogui.keyDown("command")
-        pyautogui.press("v")
-        pyautogui.keyUp("command")
-
-        password_field.send_keys(Keys.RETURN)
-
-        time.sleep(3)
-
-        write_button = driver.find_element(By.XPATH, '//*[@id="container"]/div/aside/div/div[1]/nav/a[2]')
-        action = ActionChains(driver)
-        action.move_to_element(write_button).click().perform()
-
-        input("마우스 커서를 움직여 좌표를 확인하세요")
-    finally:
-        # Driver 종료해주지 않으면 계속 쌓여서 동작 중지
-        driver.quit()
-
-# GUI 설정
-root = tk.Tk()
-root.title("Catch Cursor Point")
-
-# 화면 해상도 얻기
-screen_width, screen_height = pyautogui.size()
-
-# 창 크기 설정 및 화면 우측에 위치
-window_width = 500   # 창의 너비
-window_height = 350  # 창의 높이
-x_position = screen_width - window_width           # 화면 우측에 창 위치
-y_position = (screen_height - window_height) // 2  # 화면 중앙 높이에 위치
-
-# geometry 설정: "너비x높이+x+y"
-root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
-
-# 키 입력 Listener는 Thread로 가동
-run_listener()
-
-userId_frame = tk.Frame(root)
-userId_label = tk.Label(userId_frame, text="ID : ")
-userId_label.pack(side=tk.LEFT, padx=4)
-userId_entry = tk.Entry(userId_frame, bd=2)
-userId_entry.pack(side=tk.LEFT, padx=10)
-userId_frame.pack(pady=5)
-
-password_frame = tk.Frame(root)
-password_label = tk.Label(password_frame, text="PW : ")
-password_label.pack(side=tk.LEFT)
-password_entry = tk.Entry(password_frame, show="*", bd=2)
-password_entry.pack(side=tk.LEFT, padx=10)
-password_frame.pack(pady=5)
-
-# 네이버 블로그 Open Button
-browser_button = tk.Button(root, text="브라우저 열기", command=launch_browser)
-browser_button.pack()
-
-# 사용법 가이드 Label
-info_label = tk.Label(root, 
-                      text="F1 : 도움말 닫기 버튼\nF2 : 제목 입력 창\nF3 : 내용 입력 창\nF4 : 우측 상단 발행 버튼\nF5 : 최종 발행 버튼",
-                      borderwidth=2,
-                      relief="groove",
-                      bg="white",
-                      fg="black",
-                      anchor="w",
-                      justify="left",
-                      padx=20,
-                      pady=20)
-info_label.pack(padx=10, pady=10)
-
-# 현재 마우스 위치 표시 Label
-position_label = tk.Label(root, 
-                          text="현재 마우스 위치 : ",
-                          borderwidth=2,
-                          relief="groove",
-                          bg="white",
-                          fg="black",
-                          anchor="center",
-                          padx=20,
-                          pady=10)
-position_label.pack(padx=10, pady=10)
-
-def close_app():
-    root.destroy()
-
-button_frame = tk.Frame(root)
-# 좌표 저장 데이터 Reset Button
-reset_button = tk.Button(button_frame, text="Reset", command=reset_data)
-reset_button.pack(side=tk.LEFT, padx=5)
-# GUI Close Button
-close_button = tk.Button(button_frame, text="Close", command=close_app)
-close_button.pack(side=tk.RIGHT, padx=5)
-button_frame.pack(pady=10)
-
-# 현재 마우스 위치 Update Def
-update_mouse_position()
-
-root.mainloop()
+    window = MyWindow()
+    window.show()
+    app.exec_()
